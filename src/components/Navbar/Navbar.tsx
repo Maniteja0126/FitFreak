f325ae412169165a5099e1ae071d9084bb7908d7
Maniteja0126 <maniteja2601@gmail.com>
2024-03-09 13:23:27 +0530

    feat : added workout page and user authentication model

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,32 @@
+'use client'
 import React from 'react'
 import logo3 from '@/assets/logo3.png'
 import { IoIosBody } from 'react-icons/io'
 import './Navbar.css'
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthPopUp from '../AuthPopUp/AuthPopUp'
 
 const Navbar = () => {
+  const [isLoggedIn , setIsLoggedIn] =  React.useState<boolean>(false)
+  const [showpopup, setShowpopup] = React.useState<boolean>(false)
   return (
     <nav>
       <Image src={logo3} alt='Logo' className='logo'/>
       <Link href='/' >Home</Link>
       <Link href='/about'>About</Link>
       <Link href='/profile'> <IoIosBody /> </Link>
-      <button>Sign Out</button>
+      {
+        isLoggedIn ? 
+        <button >Logout</button>
+         : 
+        <button
+          onClick={()=>setShowpopup(true)}
+        >Login</button>
+      }
+      {
+        showpopup && <AuthPopUp setShowpopup={setShowpopup} />
+      }
     </nav>
   )
 }
